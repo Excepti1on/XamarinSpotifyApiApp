05d1b77d872e0a3d950b718b618ca378045a050e
Oliver Wölbing <oliver.woelbing@gmail.com>
2020-03-12 15:54:17 +0100

    added Tabbed Page

---
+++ XamarinSpotifyApiApp/ShortTermPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinSpotifyApiApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShortTermPage : ContentPage
+    {
+        public ShortTermPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
